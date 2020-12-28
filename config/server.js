08468788fc34bb9aa15849110020a3cf8550cd12
prakash2014cse@gmail.com <prakash2014cse@gmail.com>
2020-12-28 20:18:01 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '48386d51c18939acf2c39d36cf95ee40'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bb88f765-0729-454d-a23e-cac42c016d4d'
+};
